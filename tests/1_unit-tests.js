259058d21ad9b8395ba98945601c2c0c2f7e5792
IustinAndrei47 <160054159+IustinAndrei47@users.noreply.github.com>
2024-02-14 20:00:44 +0200

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,7 +5,6 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      /** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
   assert.isNull(null, 'this is an optional error description - e.g. null is null');
   assert.isNotNull(1, '1 is not null');
 });
